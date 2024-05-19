aa9d7ba09badb5911cf52154c9cb2c009a406401
Premysl Bednarek <premik09@seznam.cz>
2024-05-19 04:30:24 +0200

    Initial commit

---
+++ cube.py
@@ -0,0 +1,473 @@
+import numpy as np
+from typing import List, Optional
+from enum import Enum
+
+
+class Face(Enum):
+    U = 0  # up
+    F = 1  # front
+    R = 2  # right
+    B = 3  # back
+    L = 4  # left
+    D = 5  # down
+
+
+class Direction(Enum):
+    CW = 1  # clockwise
+    CCW = -1  # counter-clockwise
+    DOUBLE = 2  # double
+
+    def reverse(self) -> "Direction":
+        if self == Direction.CW:
+            return Direction.CCW
+
+        if self == Direction.CCW:
+            return Direction.CW
+
+        return Direction.DOUBLE
+
+    def to_string(self) -> str:
+        if self == Direction.CW:
+            return ""
+
+        if self == Direction.CCW:
+            return "'"
+
+        return "2"
+
+
+colors = {
+    b'W': "15m",
+    b'G': "2m",
+    b'R': "196m",
+    b'B': "4m",
+    b'O': "166m",
+    b'Y': "220m"
+}
+
+
+def get_terminal_color(color: bytes) -> str:
+    """Returns terminal escape sequence for given color.
+
+    Args:
+        color (bytes): Color string (WGRBOY).
+
+    Returns:
+        str: Terminal escape sequence.
+    """
+    assert len(color) == 1
+
+    assert color in colors.keys()
+
+    return u"\u001b[48;5;" + colors[color]
+
+
+INVERTED_DIRECTION_LAYERS = "DBLM"
+
+
+class Move:
+    def __init__(self, face: str, index: int, wide: bool, dir: Direction):
+        """Initializes the object.
+
+        Args:
+            face (str): Move face. One of U,F,R,B,L,D,M,S,E or x, y, z for
+                rotations
+            index (int): The distance from outer layer. Layer with index = 1
+                is the outer layer, the second layer has index = 2, etc.
+            wide (bool): Whether the move is wide or not.
+            dir (Direction): Move direction.
+        """
+        self.face = face
+        self.index = index
+        self.wide = wide
+        self.dir = dir
+        self.is_rotation = face in "xyz"
+        self.is_middle = self.face in "MSE"
+
+    def reverse(self) -> None:
+        """Reverses move direction.
+        """
+        self.dir = self.dir.reverse()
+
+    def get_axis(self) -> str:
+        """Returns axis of rotation.
+
+        Returns:
+            str: Axis of rotation, one of x, y, z
+        """
+        if self.face in "xyz":
+            return self.face
+        if self.face in "RML":
+            return "x"
+        if self.face in "UED":
+            return "y"
+        # self.face in "FSB"
+        return "z"
+
+    def get_layer_indices(self, n: int) -> List[int]:
+        """Returns layer indices of this move.
+
+        Args:
+            n (int): cube dimension
+
+        Returns:
+            List[int]: List of 0-indexed layer indices along the axis
+        """
+        if (self.is_middle):
+            # all inner layers
+            if (self.wide):
+                return list(range(1, n - 1))
+
+            return [n // 2]
+
+        indices = []
+        indices.append(self.index - 1)
+
+        if (self.wide):
+            if (self.index == 1):
+                indices = [0, 1]
+            else:
+                indices = list(range(self.index))
+        if (self.face in INVERTED_DIRECTION_LAYERS):
+            indices = list(map(lambda index: n - 1 - index, indices))
+        return indices
+
+    @staticmethod
+    def from_string(move: str) -> "Move":
+        """Initialize the move from a string.
+
+        Args:
+            move (str): Move string in Rubik's cube notation.
+
+        Returns:
+            Move: A Move object.
+        """
+        i = 0
+        layer_index = 0
+        while move[i].isdigit():
+            layer_index *= 10
+            layer_index += int(move[i])
+            i += 1
+
+        # first layer index is implicit
+        if (layer_index == 0):
+            layer_index = 1
+
+        face = move[i]
+        i += 1
+
+        wide = i < len(move) and move[i] == 'w'
+        if (wide):
+            i += 1
+
+        if face.islower() and face not in "xyz":
+            wide = True
+            face = face.upper()
+
+        if i < len(move) and move[i] == '2':
+            dir = Direction.DOUBLE
+        elif i < len(move) and move[i] == "'":
+            dir = Direction.CCW
+        else:
+            dir = Direction.CW
+
+        return Move(
+            face=face,
+            index=layer_index,
+            wide=wide,
+            dir=dir
+        )
+
+
+class Cube:
+    def __init__(self, n: int, state: Optional[bytes] = None):
+        """Create a Cube object.
+
+        Args:
+            n (int): Cube size
+            state (bytes, optional): Cube state obtained with the .serialize
+                method. If no state is passed, a cube with default state will
+                be created. Default state has the white color facing up
+                and green facing front.
+        """
+        self.n = n
+
+        # for each sticker, create a array element
+        # this flat array will be used for serialization of the cube state
+        if state is None:
+            self.flat = np.zeros(n * n * 6, dtype=np.dtype("S1"))
+        else:
+            self.flat = np.frombuffer(state, np.dtype("S1")).copy()
+
+        # self.faces is a numpy view, that means any changes in self.faces
+        # will reflected in self.flat and vice versa
+        # self.faces will be used for indexing the cube
+        # self.faces[i] is nxn array
+        self.faces = self.flat.reshape(6, n, n)
+
+        if state is None:
+            for i, color in enumerate(colors.keys()):
+                self.faces[i] = color
+
+    def serialize(self) -> bytes:
+        """Returns serialized inner cube state, which can be used to init
+        this object (either in constructor or .deserialize method).
+
+        Returns:
+            bytes: Serialized cube state.
+        """
+        return self.flat.tobytes()
+
+    def deserialize(self, buffer: bytes) -> None:
+        """Initialize inner cube state from serialized state.
+
+        Args:
+            buffer (bytes): Cube state, which was obtained with the .serizalize
+                method.
+        """
+        arr = np.frombuffer(buffer, np.dtype("S1"))
+        self.flat[:] = arr.copy()
+
+    def _get_face(self, face: Face) -> np.ndarray:
+        """Gets cube face.
+
+        Args:
+            face (Face): Face object specifying the face.
+
+        Returns:
+            np.ndarray: ndarray of stickers with self.n * self.n dimensions
+        """
+        return self.faces[face.value]
+
+    def pprint(self) -> None:
+        """
+        Pretty print the cube to stdout with colors.
+
+        The cube is "unfolded" and printed in the following format:
+
+              W W W
+              W W W
+              W W W
+        O O O G G G R R R B B B
+        O O O G G G R R R B B B
+        O O O G G G R R R B B B
+              Y Y Y
+              Y Y Y
+              Y Y Y
+        """
+
+        table_height = 3 * self.n
+        table_width = 4 * self.n
+        print_table: np.chararray = np.chararray((table_height, table_width))
+        print_table[:] = ''
+
+        # subdivide the print_table into 3x4 grid of n*n matrices
+        def fill(row: int, col: int, face: Face):
+            row_start = row * self.n
+            col_start = col * self.n
+            print_table[
+                row_start: row_start + self.n,
+                col_start: col_start + self.n
+            ] = self._get_face(face)
+
+        # fill the print_table with corresponding stickers
+        fill(0, 1, Face.U)
+        fill(1, 0, Face.L)
+        fill(1, 1, Face.F)
+        fill(1, 2, Face.R)
+        fill(1, 3, Face.B)
+        fill(2, 1, Face.D)
+
+        for line in print_table:
+            for elem in line:
+                if elem == '':
+                    print(" ", end='')
+                    continue
+
+                print(
+                    get_terminal_color(elem),
+                    end='')  # color escape sequence
+                print(elem.decode('UTF-8'), end='')     # color letter
+                print(u"\u001b[0m", end='')             # reset terminal color
+
+            print()
+
+    def _rotate_face(self, face: Face, dir: Direction) -> None:
+        """Rotate stickers on a face.
+
+        This only affects stickers on the given face, thereby only self.n^2
+        stickers are affected.
+
+        Args:
+            face (Face): A face that will be rotated.
+            dir (Direction): Direction of rotation.
+        """
+        f = self._get_face(face)
+        # passing dir = 1 to rot90 rotates anticlockwise, thus the reverse
+        f[:] = np.rot90(f, dir.reverse().value)
+
+    def _get_layer_stickers(self, axis: str, i: int) -> List[np.ndarray]:
+        """Gets stickers on given layer.
+
+        Args:
+            axis (str): Axis on which we choose the layer.
+            i (int): Index of the layer. For example, the left outer layer
+                has index 0 and right outer layer has index of cube_size - 1
+
+        Returns:
+            List[np.ndarray]: List of views, each view includes stickers
+                from one face.
+        """
+        assert axis in "xyz"
+        if axis == "x":
+            return [
+                self._get_face(Face.U)[:, -1 - i][::-1],
+                self._get_face(Face.B)[:, i],
+                self._get_face(Face.D)[:, -1 - i][::-1],
+                self._get_face(Face.F)[:, -1 - i][::-1],
+            ]
+        if axis == "y":
+            return [
+                self._get_face(Face.F)[i],
+                self._get_face(Face.L)[i],
+                self._get_face(Face.B)[i],
+                self._get_face(Face.R)[i]
+            ]
+
+        return [
+            self._get_face(Face.U)[-1 - i],
+            self._get_face(Face.R)[:, i],
+            self._get_face(Face.D)[i][::-1],
+            self._get_face(Face.L)[:, -1 - i][::-1]
+        ]
+
+    @staticmethod
+    def _cycle_views(views: List[np.ndarray], dir: Direction):
+        """Rotates a list of views (or simple ndararys) in specified direction.
+
+        Args:
+            views (List[np.ndarray]): List of ndarrays or views to rotated.
+            dir (Direction): Rotation direction.
+        """
+        rotated = np.roll(views, dir.value, 0)
+        for rotated, view in zip(rotated, views):
+            view[:] = rotated
+
+    def _rotate_layer(self, axis: str, index: int, dir: Direction, face: str):
+        """Rotates one layer of the cube.
+
+        Args:
+            axis (str): An axis, around which we rotate.
+            index (int): Distance of the rotated layer from the outer layer.
+            dir (Direction): Direction of the rotation.
+            face (str): A face, to which the layer belongs U/F/R/B/L/D/M/S/E
+        """
+
+        # the layer can move in other direction if the rotation is inverted
+        # for example, CW clockwise of R and L face will result in the same
+        # rotation of face stickers, but the stickers on other faces will
+        # rotate in opposite directions
+        layer_dir = dir
+        if face in INVERTED_DIRECTION_LAYERS:
+            layer_dir = layer_dir.reverse()
+        self._cycle_views(self._get_layer_stickers(axis, index), layer_dir)
+
+        # if the layer is an outer layer, move also the stickers of the face
+        if (index == 0 or index == self.n - 1):
+            self._rotate_face(Face[face], dir)
+
+    def _perform_rotation(self, rotation: Move):
+        """Performs a cube rotation - x, y, z moves.
+
+        Args:
+            rotation (Move): Move objects, that is a rotation.
+        """
+        assert rotation.is_rotation
+
+        if rotation.face == "x":
+            # stickers on these face swap places
+            views = [
+                self._get_face(Face.U),
+                np.rot90(self._get_face(Face.B), 2),
+                self._get_face(Face.D),
+                self._get_face(Face.F)
+            ]
+            # f1, f2 only get rotated
+            f1 = Face.R
+            f2 = Face.L
+
+        if rotation.face == "y":
+            views = [
+                self._get_face(Face.F),
+                self._get_face(Face.L),
+                self._get_face(Face.B),
+                self._get_face(Face.R)
+            ]
+            f1 = Face.U
+            f2 = Face.D
+
+        if rotation.face == "z":
+            views = [
+                self._get_face(Face.U),
+                np.rot90(self._get_face(Face.R), 1),
+                self._get_face(Face.D)[::-1, ::-1],
+                np.rot90(self._get_face(Face.L), 3)
+            ]
+            f1 = Face.F
+            f2 = Face.B
+
+        self._cycle_views(views, rotation.dir)
+        self._rotate_face(f1, rotation.dir)
+        self._rotate_face(f2, rotation.dir.reverse())
+
+    def _single_move(self, move_str: str):
+        """Performs a single move on the cube.
+
+        Args:
+            move_str (str): Move in Rubik's cube notation.
+        """
+        move: Move = Move.from_string(move_str)
+
+        if move.is_rotation:
+            self._perform_rotation(move)
+            return
+
+        for index in move.get_layer_indices(self.n):
+            self._rotate_layer(
+                move.get_axis(),
+                index,
+                move.dir,
+                move.face,
+            )
+
+    def move(self, moves_str: str) -> "Cube":
+        """Performs a move or sequence of moves on the cube.
+
+        Args:
+            moves_str (str): A single move a multiple moves in Rubik's cube
+                notation separated by whitespace.
+
+        Returns:
+            Cube: self
+        """
+        moves: List[str] = moves_str.split()
+        for move_str in moves:
+            self._single_move(move_str)
+
+        return self
+
+    def is_solved(self) -> bool:
+        """Checks whether the cube is solved.
+
+        Returns:
+            bool: true if the cube is solved, false otherwise.
+        """
+        for face in self.faces:
+            # check whether all stickers on the face are of the same color
+            if not (face == face[0][0]).all():
+                return False
+        return True
+
+
+
+
---
+++ cube_tests.py
@@ -0,0 +1,221 @@
+import unittest
+from cube import Move, Cube, Direction, Face
+import numpy as np
+
+# reconstruction of
+# 1:47.87 WR 7x7 solve by Max Park at West Coast Cubing Tour - Fresno 2018
+# source:
+# https://www.speedsolving.com/threads/max-park-7x7-1-47-89-single-1-57-10-mean.70316/#post-1296768
+wr7x7scramble = "L' b L2 R' 3f f2 F2 3d u F' L 3f B2 3d R2 D2 3r' b2 D' f' D2 f' D2 3l' b F B l2 3f' 3d2 L R2 3d2 3b2 B2 L' d' b' R2 B l' L' F' U' 3u2 d2 b' 3l2 R U' 3b f 3f' d D' U2 f2 R2 L2 D 3r U 3f2 3b 3r' d U' B2 F2 f l R u' R L' d2 u2 3u2 f2 R d b2 f U2 L 3f r' 3u' D' r D2 U 3f2 L2 U2 3b 3u r B' R"
+solve = """
+z y2 // inspection
+ // centres
+ // blue
+r U' r' 4r 3r' // line
+(z y) x' U 4r 3r' U 4r' 3r 3r2' r2 // line
+U 5r U (x' z') U' 5r' 4r u' // line
+x U x U' 3r x' 5r2' 4r2 (x' z') U 4r' // line
+y 4r' 3r l' U l x' (y x) U' x U r' U 5l D' (r' 5l') // blue
+ // green
+U 5l U r' 3r' r U' 3r r' U // line
+x r' U r U 3r' r U' 3r r' (x' z') U' 4r' 3r U' 3r' r z 3l' U U 3l // line
+z' F U' U' 4l U' r' U U x' 4r x' U' (x z) u u r U' U' r' // line
+(z x') F 5r' F 4r' U' x r2' z' U' U' r U' U' r' // line
+(z x') 5r U 5r' x' 3r' r x U 5r' 4r z' U' 3r r' U2 3r' r // green
+ // white
+z r U' U' x x' D' (3r 4l) U' r' // line
+F' U' 4r' U' U' x 3r' r U l // line
+U 3r r' U' 3r' r U 3r r' U' U' x' U' U x' (r' 5l') F r2 5r' 4r // line
+U' 3r U' U' 3r' 3r2' r2 U' 3r2' r2 // line
+3r' r U 3r r' U' U' x' U U' x U 3r r' U' 3r' r U' U' r F r U' r2' // white
+ // red
+F U U 4l' U' x' 3r // line
+U l' U l // line
+U r U' r' 5r 4r' U l' 3l U' 5r 4r' U' 5r' 4r // line
+3l U 4r' 4r U' U' 4r' x r U' r' U r U r' // line
+U' x' U' 5l' U U 5l U x 4r' 3r 3r' r U 4r 3r' 3r r' 3r r' U2 3r' r // red
+ // yellow / orange
+(U x') F U' r' F r // line
+U' U' U r r' U' r U r' r U' 4r 3r' U2 4r' 3r U 5l' // line
+3r' r U U 4l r' // line
+3r U' 3r' U U' r U' U' r' U r U r' U 3r U' U' 3r' // line
+r r' U r U r' U' r U' U' r' //
+U 5l' U' 3r' r U r U' 3r r' //
+5r' 4r U r' U' 5r 4r' U r // yellow / orange
+ // edges
+ // F8E
+U' z' U' R U' R' u R U R' F R' F' R 3u' d U' L' U L 3d d' R U R' F R' F' R u' // WG
+U R U' R' u z' 4r' 3r z' R U R' F R' F' R z' 4r' 3r z 3d' d F R' F' R u // WR
+L' U L d (U' y) U' R U' R' u F R' F' R 3u u' R U' R' U' 5u' // WB
+U R U' z' U' 3r' r z' d R U R' F R' F' R u d' R U' R' z' 3r' r // WO
+z u u' U U U' L' U L u R U R' F R' F' R u' 6d' F R' F' R 4u' U y R U' R' d' // RB
+U' L' U L u u U F R' F' R (x' y') 3r r' z R U R' F R' F' R u F R' R R' F' R z' x 4r' 3r // OB
+z R U' R' z2' U R U' R' d (y z') 4r' 3r 4r' 3r // YB
+z' U' 6d' L' U L (x y') 4r2' (4r 3r) 4r' 3r 4r' z' R U R' F R' F' R u' U y 5u' // YR
+ // L4E
+U R U' R' z' 4r 3r2' r2 // fix centres
+x' U2 (3r 4l) z R U R' F R' F' R z' 3r2' //
+r' z' R U R' F R' F' R u // YG
+z' x' 3r' r z' R U R' F R' F' R z 3r r' //
+U2 U U' 5r' z' R U R' F R' F' R d // GO
+z' 3r' z' R U R' F R' F' R 3u //
+5d R U R' F R' F' R u' // YO
+y z' 3r U' U' 3r x U U 3r U' U' 4r' U U 3l U' U' 3r' U U 3r U' U' 3r' U U 4l' // GR
+ // 3x3x3
+U' 6r' D x' D R D F F D // yellow cross
+y R U' R2' U R // yRB
+y' R' R2 U' R2' U R2 // yGR
+U R' U R U' R' // uGO
+L' U U L U' L' U L // yOB
+U' R' U' R U' R' U R U x' R U' R' U // OLL
+x 6l' U R' D D R U' R' D D R2 x' U // PLL
+"""
+
+lines = list(map(lambda x: x.split("/", 1)[0], solve.splitlines()))
+
+for i in range(len(lines)):
+    lines[i] = "".join(list(filter(lambda x: x not in "()", lines[i])))
+
+wr7x7solve = " ".join(lines)
+
+
+class TestClass(unittest.TestCase):
+    def test_wide(self):
+        self.assertTrue(Move.from_string("1212Uw2'").wide)
+        self.assertFalse(Move.from_string("1212U2'").wide)
+
+    def test_cw(self):
+        self.assertEqual(Move.from_string("1212Uw2").dir, Direction.DOUBLE)
+        self.assertEqual(Move.from_string("1212Uw2'").dir, Direction.DOUBLE)
+
+    def test_index(self):
+        self.assertEqual(Move.from_string("1212Uw2").index, 1212)
+        self.assertEqual(Move.from_string("Uw2").index, 1)
+        self.assertEqual(Move.from_string("2'").index, 2)
+
+    def test_double(self):
+        self.assertEqual(Move.from_string("1212Uw2").dir, Direction.DOUBLE)
+        self.assertNotEqual(Move.from_string("Uw").dir, Direction.DOUBLE)
+
+    def test_full(self):
+        out = Move.from_string("17Rw2'")
+        self.assertEqual(out.index, 17)
+        self.assertEqual(out.face, "R")
+        self.assertTrue(out.wide)
+        self.assertEqual(out.dir, Direction.DOUBLE)
+
+    def test_full2(self):
+        out = Move.from_string("L")
+        self.assertEqual(out.index, 1)
+        self.assertEqual(out.face, "L")
+        self.assertFalse(out.wide)
+        self.assertNotEqual(out.dir, Direction.DOUBLE)
+
+    def test_index2(self):
+        move = Move.from_string("U")
+        self.assertEqual(move.get_layer_indices(3), [0])
+        self.assertEqual(move.get_layer_indices(5), [0])
+        move = Move.from_string("3U")
+        self.assertEqual(move.get_layer_indices(9), [2])
+        move = Move.from_string("2U")
+        self.assertEqual(move.get_layer_indices(9), [1])
+
+    def test_index3(self):
+        for layer in "LDB":
+            move = Move.from_string(layer)
+            self.assertEqual(move.get_layer_indices(3), [2])
+            self.assertEqual(move.get_layer_indices(5), [4])
+            move = Move.from_string(str(3) + layer)
+            self.assertEqual(move.get_layer_indices(11), [8])
+
+    def test_middle(self):
+        for layer in "mse":
+            move = Move.from_string(layer)
+            self.assertEqual(move.get_layer_indices(3), [1])
+            self.assertEqual(move.get_layer_indices(5), [1, 2, 3])
+            self.assertEqual(move.get_layer_indices(7), [1, 2, 3, 4, 5])
+        for layer in "MSE":
+            move = Move.from_string(layer)
+            self.assertEqual(move.get_layer_indices(3), [1])
+            self.assertEqual(move.get_layer_indices(5), [2])
+            self.assertEqual(move.get_layer_indices(7), [3])
+
+    def test_move_basic(self):
+        c = Cube(3)
+        # order of an algoritm is the number of times we have to perform it
+        # before the cube comes to the original state
+        # algorithm can be viewed as a permutation
+        alg = "U D L' U' F B' F' R D' L R' D' B"
+
+        # order calculated with https://mzrg.com/rubik/ordercalc.shtml
+        # beware: the calculator uses SiGN notation
+        order = 120
+        c.move(alg)
+        for i in range(order - 1):
+            self.assertFalse(c.is_solved())
+            c.move(alg)
+        self.assertTrue(c.is_solved())
+
+    def test_solve(self):
+        c = Cube(3)
+        solve = "D F D' F U' R U F' L L' F' B' B' L' B L L B D U L L' D B R y' x' z' z' y' y' x' x' x' y' R' U' U' F' F' U' R' F R D' y' U R' U R R U R' y' U U y' R' U R y' R' U' R U U y' R' U' R U' y' L U L' U' R U R' U' U' R U' U' R' U y' R' U' R y' y' y' F R U R' U' F' U U' L' U' U' L U L' U L U U U U R U R' F' R U R' U' R' F R R U' R' U U U R R U R U R' U' R' U' R' U R' U' U' "
+        c.move(solve)
+        self.assertTrue(c.is_solved())
+
+    def test_cycle_pos(self):
+        a = np.arange(12).reshape(4, 3)
+        views = [a[0], a[1], a[2], a[3]]
+        Cube._cycle_views(views, Direction.CW)
+        exp = np.roll(np.arange(12), 3).reshape(4, 3)
+        self.assertTrue(np.array_equiv(a, exp))
+
+    def test_cycle_neg(self):
+        a = np.arange(12).reshape(4, 3)
+        views = [a[0], a[1], a[2], a[3]]
+        Cube._cycle_views(views, Direction.CCW)
+        exp = np.roll(np.arange(12), -3).reshape(4, 3)
+        self.assertTrue(np.array_equiv(a, exp))
+
+    def test_cycle_double(self):
+        a = np.arange(12).reshape(4, 3)
+        views = [a[0], a[1], a[2], a[3]]
+        Cube._cycle_views(views, Direction.DOUBLE)
+        exp = np.roll(np.arange(12), 6).reshape(4, 3)
+        self.assertTrue(np.array_equiv(a, exp))
+
+    def test_double_(self):
+        c = Cube(3)
+        # order of an algoritm is the number of times we have to perform it
+        # before the cube comes to the original state
+        # algorithm can be viewed as a permutation
+        alg = "R2 U2 L2"
+
+        # order calculated with https://mzrg.com/rubik/ordercalc.shtml
+        # beware: the calculator uses SiGN notation
+        order = 4
+        c.move(alg)
+        for i in range(order - 1):
+            self.assertFalse(c.is_solved())
+            c.move(alg)
+        self.assertTrue(c.is_solved())
+
+    def test_wide_(self):
+        c = Cube(7)
+        c.move("4Rw 3Lw'")
+        c2 = Cube(7)
+        c2.move("x")
+        self.assertTrue(np.array_equal(c.flat, c2.flat))
+
+    def test_wide_small_(self):
+        c = Cube(3).move("r R' M")
+        self.assertTrue(c.is_solved())
+
+    def test_big(self):
+        c = Cube(7)
+        c.move(wr7x7scramble)
+        c.move(wr7x7solve)
+        self.assertTrue(c.is_solved())
+
+
+if __name__ == '__main__':
+    unittest.main()
